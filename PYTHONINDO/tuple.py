c555e887bbad2ff82327fd80d8347821dcf4d79a
sopan setiawan <setiawan.sopan@gmail.com>
2021-01-22 07:59:19 +0700

    22 Januari 2021

---
+++ PYTHONINDO/tuple.py
@@ -0,0 +1,10 @@
+tuple1 = (1,2,3,4,5,6,7,8,9)
+print(3 in tuple1)
+print('s' not in tuple1)
+
+tuple2 = ('sopan', 'umi', 'finda', 'muthi', 'ahsan')
+for nama in tuple2:
+    print('Hi', nama)
+
+print(tuple2.count('umi'))
+print(tuple2.index('finda'))
